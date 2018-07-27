7a9564be9a1fb2b7fad07dcf4ce693f6d73a12fe
Allan <sattelberg-rivera.1@osu.edu>
2018-07-27 18:37:12 -0400

    Added LocalLibrary files to git repository

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/cool', function(req,res,next){
+	res.send('You\'re so cool you exhale ice cubes');
+});
+module.exports = router;
